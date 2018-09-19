327bbd1bc99e8dd70fd63b5e07dcc69e53be7e52
Maximo Sojo <maxsojo13@gmail.com>
2018-09-18 23:24:20 -0400

    refactor(UserTrait): Se registra evento de registro de usuario.

---
+++ Traits/UserEventTrait.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+ * This file is part of the Atechnologies package.
+ * 
+ * (c) www.atechnologies.com.ve
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Atechnologies\ToolsBundle\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use AppBundle\Entity\User;
+
+/**
+ * Add Blameable behavior to an entity.
+ * @author Máximo Sojo <maxsojo13@gmail.com>
+ */
+trait UserEventTrait 
+{
+    /**
+     * @var User
+     *
+     * @Gedmo\Blameable(on="create")
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumn(name="created_by", referencedColumnName="id")
+     */
+    protected $createdBy;
+
+    /**
+     * @var User
+     *
+     * @Gedmo\Blameable(on="update")
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumn(name="updated_by", referencedColumnName="id")
+     */
+    protected $updatedBy;
+
+    /**
+     * Set createdBy
+     *
+     * @param User $createdBy
+     * @return Object
+     */
+    public function setCreatedBy(User $createdBy)
+    {
+        $this->createdBy = $createdBy;
+
+        return $this;
+    }
+
+    /**
+     * Get createdBy
+     *
+     * @return User
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * Set updatedBy
+     *
+     * @param User $updatedBy
+     * @return Object
+     */
+    public function setUpdatedBy(User $updatedBy)
+    {
+        $this->updatedBy = $updatedBy;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedBy
+     *
+     * @return User
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updatedBy;
+    }
+}
