5f69c0debedc3065fccdb38b232758cbcba0aede
Maximo Sojo <maxsojo13@gmail.com>
2018-10-20 20:05:44 -0400

    refactor(SequenceGeneratorBase): Se registra base para geneador de secuencia.

---
+++ Service/SequenceGenerator/SequenceGeneratorBase.php
@@ -0,0 +1,88 @@
+<?php
+
+/*
+ * This file is part of the TecnoCreaciones package.
+ * 
+ * (c) www.tecnocreaciones.com
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Atechnologies\ToolsBundle\Service\SequenceGenerator;
+
+use Doctrine\Common\Util\ClassUtils;
+use LogicException;
+use AppBundle\Service\SequenceGenerator\ItemReferenceInterface;
+
+/**
+ * Base del generador de secuencias
+ *
+ * @author Carlos Mendoza <inhack20@gmail.com>
+ */
+abstract class SequenceGeneratorBase implements SequenceGeneratorBaseInterface
+{
+    /**
+     * Instancia del generador de secuencias
+     * @var \Atechnologies\ToolsBundle\Service\SequenceGenerator\SequenceGeneratorService
+     */
+    protected $sequenceGenerator;
+    
+    /**
+     * Construye la referencia por defecto
+     * @param ItemReferenceInterface $item
+     * @param array $config
+     * @return type
+     */
+    public function buildRef(ItemReferenceInterface $item,array $config) 
+    {
+        $mask = $config['mask'];
+        $className = $config['className'];
+        $field = $config['field'];
+        $qb = $this->sequenceGenerator->createQueryBuilder();
+        $qb->from($className,'p');
+        return $this->sequenceGenerator->generateNext($qb, $mask,$field,[],$config);
+    }
+    
+    /**
+     * Establece la referencia aun objeto
+     * @param ItemReferenceInterface $item
+     * @return type
+     * @throws LogicException
+     */
+    public function setRef(ItemReferenceInterface $item) 
+    {
+        $className = ClassUtils::getRealClass(get_class($item));
+        
+        $classMap = $this->getClassMap();
+        if(!isset($classMap[$className])){
+            throw new LogicException(sprintf("No ha definido la configuracion de '%s' para generar su referencia",$className));
+        }
+        $resolver = new \Symfony\Component\OptionsResolver\OptionsResolver();
+        $resolver->setDefined([
+            "method","field","options","mask"
+        ]);
+        $resolver->setDefaults([
+            'method' => 'buildRef',
+            'field' => 'ref',
+            'use_cache' => false,
+//            'options' => array()
+        ]);
+        
+        $config = $resolver->resolve($classMap[$className]);
+        $config['className'] = $className;
+        $method = $config['method'];
+        $ref = $this->$method($item,$config);
+        $item->setRef($ref);
+        return $ref;
+    }
+    
+    /**
+     * Establece el generador de secuencia
+     * @param \Atechnologies\ToolsBundle\Service\SequenceGenerator\SequenceGeneratorService $sequenceGenerator
+     */
+    function setSequenceGenerator(\Atechnologies\ToolsBundle\Service\SequenceGenerator\SequenceGeneratorService $sequenceGenerator) 
+    {
+        $this->sequenceGenerator = $sequenceGenerator;
+    }
+}
