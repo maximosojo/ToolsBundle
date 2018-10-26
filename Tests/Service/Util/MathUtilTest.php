da71e46f7022a2258e7316c7ed317d99a913dde9
Maximo Sojo <maxsojo13@gmail.com>
2018-10-26 16:37:31 -0400

    refactor(MathUtil): Se registra test de util para Math.

--- Tests/Service/Util/MathUtilTest.php
+++ Tests/Service/Util/MathUtilTest.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+ * This file is part of the Máximo Sojo - maxtoan package.
+ * 
+ * (c) https://maxtoan.github.io/
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Atechnologies\ToolsBundle\Tests\Service\Util;
 
@@ -13,5 +21,29 @@ class MathUtilTest extends \PHPUnit_Framework_TestCase
 		$b = 3;
 		$sum = MathUtil::sum($a,$b);
 		$this->assertEquals(4, $sum);
+	}
+
+	public function testSub()
+	{
+		$a = 3;
+		$b = 1;
+		$sub = MathUtil::sub($a,$b);
+		$this->assertEquals(2, $sub);
+	}
+
+	public function testDiv()
+	{
+		$a = 1;
+		$b = 3;
+		$div = MathUtil::div($a,$b);
+		$this->assertEquals(0.33, $div);
+	}
+
+	public function testMul()
+	{
+		$a = 1;
+		$b = 3;
+		$mul = MathUtil::mul($a,$b);
+		$this->assertEquals(3, $mul);
 	}
 }
