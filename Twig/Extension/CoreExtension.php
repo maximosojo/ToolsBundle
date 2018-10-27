f224038e3abfb352724a74faa7d985436d79b5ce
Maximo Sojo <maxsojo13@gmail.com>
2018-10-27 11:59:13 -0400

    refactor(CoreExtension): Se realiza optimización de extensión twig en core.

--- Twig/Extension/CoreExtension.php
+++ Twig/Extension/CoreExtension.php
@@ -14,7 +14,7 @@ namespace Atechnologies\ToolsBundle\Twig\Extension;
 /**
  * Extension generic applications
  * 
- * @author Máximo Sojo maxsojo13@gmail.com <maxtoan at atechnologies>
+ * @author Máximo Sojo <maxsojo13@gmail.com>
  */
 class CoreExtension extends \Twig_Extension 
 {    
@@ -29,13 +29,7 @@ class CoreExtension extends \Twig_Extension
     public function getFunctions() 
     {
        return array(
-            new \Twig_SimpleFunction('form_top', null, array('node_class' => 'Symfony\Bridge\Twig\Node\SearchAndRenderBlockNode', 'is_safe' => array('html'))),
-            new \Twig_SimpleFunction('print_error', array($this, 'printError'), array('is_safe' => array('html'))),
-            new \Twig_SimpleFunction('breadcrumb', array($this, 'breadcrumb'), array('is_safe' => array('html'))),
-            new \Twig_SimpleFunction('generateLink', array($this, 'generateLink'), array('is_safe' => array('html'))),
-            new \Twig_SimpleFunction('generateLinkUrlOnly', array($this, 'generateLinkUrlOnly'), array('is_safe' => array('html'))),
-            new \Twig_SimpleFunction('call_static_method', array($this, 'call_static_method')),
-            new \Twig_SimpleFunction('get_next_item', array($this, 'getNextItem'))
+            new \Twig_SimpleFunction('breadcrumb', array($this, 'breadcrumb'), array('is_safe' => array('html')))
         );
     }
     
@@ -48,12 +42,7 @@ class CoreExtension extends \Twig_Extension
     public function getFilters() 
     {
         return array(
-            new \Twig_SimpleFilter('str_pad', function($input, $padlength, $padstring = '', $padtype = STR_PAD_LEFT) {
-                        return str_pad($input, $padlength, $padstring, $padtype);
-                    }),
             new \Twig_SimpleFilter('myNumberFormat', array($this, 'myNumberFormat')),
-            new \Twig_SimpleFilter('ucwords', array($this, 'ucwords')),
-            new \Twig_SimpleFilter('is_string', array($this, 'is_string')),
             new \Twig_SimpleFilter('myFormatDateTime', array($this, 'myFormatDateTime')),
             new \Twig_SimpleFilter('myFormatDate', array($this, 'myFormatDate'))
         );
@@ -119,55 +108,12 @@ class CoreExtension extends \Twig_Extension
     }
 
     /**
-     * Filtro para Mayusculas en Cada palabra de un String.
-     * @param type $value
-     * @param type $string
-     * @return type
-     */
-    function ucwords($value) 
-    {
-        return ucwords(mb_strtolower($value, 'UTF-8'));
-    }
-
-    /**
-     * Filtro para Determinar si la Entrada es String o No
-     * @param type $value
-     * @return type
-     */
-    function is_string($value) 
-    {
-        return is_string($value);
-    }
-
-    /**
-     * Genera un link completo para mostrar el objeto
-     * 
-     * @param type $entity
-     * @param type $type
-     * @return type
-     */
-    function generateLink($entity, $type = \Atechnologies\ToolsBundle\Service\LinkGenerator\LinkGeneratorService::TYPE_LINK_DEFAULT, array $parameters = array()) 
-    {
-        return $this->container->get('atechnologies.service.link_generator')->generate($entity, $type, $parameters);
-    }
-
-    /**
-     * Genera solo la url de el objeto
-     * 
-     * @param type $entity
-     * @param type $type
-     * @return type
-     */
-    function generateLinkUrlOnly($entity, $type = \Atechnologies\ToolsBundle\Service\LinkGenerator\LinkGeneratorService::TYPE_LINK_DEFAULT, array $parameters = array()) 
-    {
-        return $this->container->get('atechnologies.service.link_generator')->generateOnlyUrl($entity, $type, $parameters);
-    }
-
-    /**
-     * This is a cool function
-     * @author Máximo Sojo maxsojo13@gmail.com <maxtoan at atechnologies>
-     * @param  [type]
-     * @return [type]
+     * Formateo de fecha en formato DateTime
+     *  
+     * @author Máximo Sojo <maxsojo13@gmail.com>
+     * @param  $myFormatDate [Fecha a formatear]
+     * @param  string $format       [Formato esperado]
+     * @return DateTime
      */
     public function myFormatDateTime($myFormatDate, $format = "d-m-Y h:i a") 
     {
@@ -179,10 +125,12 @@ class CoreExtension extends \Twig_Extension
     }
 
     /**
-     * This is a cool function
-     * @author Máximo Sojo maxsojo13@gmail.com <maxtoan at atechnologies>
-     * @param  [type]
-     * @return [type]
+     * Formateo de fecha
+     *  
+     * @author Máximo Sojo <maxsojo13@gmail.com>
+     * @param  $myFormatDate [Fecha a formatear]
+     * @param  string $format       [Formato esperado]
+     * @return Date
      */
     public function myFormatDate($myFormatDate, $format = "d-m-Y") 
     {
@@ -191,53 +139,6 @@ class CoreExtension extends \Twig_Extension
             $dateFormated = $myFormatDate->format($format);
         }
         return $dateFormated;
-    }
-
-    /**
-     * This is a cool function
-     * @author Máximo Sojo maxsojo13@gmail.com <maxtoan at atechnologies>
-     * @param  [type]
-     * @param  [type]
-     * @param  array
-     * @return [type]
-     */
-    public function call_static_method($object, $method, array $args) 
-    {
-        return call_user_func_array(array($object, $method), $args);
-    }
-
-    /**
-     * This is a cool function
-     * @author Máximo Sojo maxsojo13@gmail.com <maxtoan at atechnologies>
-     * @param  [type]
-     * @param  [type]
-     * @return [type]
-     */
-    public function getNextItem($loop, $items) 
-    {
-        $nextItem = null;
-        if ($loop['length'] > 1) {
-            if ($loop['last'] == false) {
-                $nextItem = $items[$loop['index0'] + 1];
-            }
-        }
-        return $nextItem;
-    }
-
-    /**
-     * Returns the name of the extension.
-     *
-     * @return string The extension name
-     */
-    public function getName() 
-    {
-        return 'humper_core_extension';
-    }
-
-    function generateAsset($path, $packageName = null) 
-    {
-        return $this->container->get('templating.helper.assets')
-                        ->getUrl($path, $packageName);
     }
 
     /**
@@ -297,5 +198,15 @@ class CoreExtension extends \Twig_Extension
     public function getConfiguration() 
     {
         return $this->container->get("atechnologies.service.configuration");
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName() 
+    {
+        return 'atechnologies_core_extension';
     }
 }
