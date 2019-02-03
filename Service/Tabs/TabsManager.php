a166fda99979c624becfcd0164d5abc3b9bb14e2
Maximo Sojo <maxsojo13@gmail.com>
2019-02-03 16:52:01 -0400

    refactor(Tabs): Se registra manejadors de tabs.

---
+++ Service/Tabs/TabsManager.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Grupo Farmaingenio C.A. - J406111090 package.
+ * 
+ * (c) www.tconin.com
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Atechnologies\ToolsBundle\Service\Tabs;
+
+use Atechnologies\ToolsBundle\Model\Core\Tab\Tab;
+use Atechnologies\ToolsBundle\Model\Core\Tab\TabContent;
+
+/**
+ * Servicio generador de tabs (atechnologies.manager.tabs)
+ * @author Máximo Sojo <maxsojo13@gmail.com>
+ */
+class TabsManager extends \Atechnologies\ToolsBundle\Service\BaseService 
+{
+    /**
+     * $tab
+     * @var Tab
+     */
+    private $tab;
+
+    /**
+     * Instancia nueva tabs
+     * @author Máximo Sojo <maxsojo13@gmail.com>
+     * @param  Class
+     * @return Tab
+     */
+    public function createNew($class = null)
+    {
+        $this->tab = new Tab([]);
+
+        return $this->tab;
+    }
+
+    /**
+     * Instancia nuevo contenido de tab
+     * @author Máximo Sojo <maxsojo13@gmail.com>
+     * @param  array
+     * @return TabContent
+     */
+    public function createNewContent(array $options = array())
+    {
+        return new TabContent($options);
+    }
+
+    /**
+     * Registra contenido en tab
+     * @author Máximo Sojo <maxsojo13@gmail.com>
+     * @param  TabContent
+     */
+    public function addTabContent(TabContent $tabContent)
+    {
+        $this->tab->addTabContent($tabContent);
+    }
+}
