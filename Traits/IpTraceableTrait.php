2619414095c04473fe3e6e3bfc63cfdbb5e71132
Maximo Sojo <maxsojo13@gmail.com>
2019-02-06 16:11:52 -0400

    feat(IpTraceableTrait): Se registra trait para registro de ip de acciones.

---
+++ Traits/IpTraceableTrait.php
@@ -0,0 +1,85 @@
+<?php
+
+/*
+ * This file is part of the Atechnologies package.
+ * 
+ * (c) www.atechnologies.com.ve
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Atechnologies\ToolsBundle\Traits;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * IpTraceableTrait
+ *
+ * @author Máximo Sojo <maxsojo13@gmail.com> 
+ */
+trait IpTraceableTrait 
+{
+   /**
+     * @var string $createdIp
+     *
+     * @Gedmo\IpTraceable(on="create")
+     * @ORM\Column(type="string", name="created_ip",length=45, nullable=true)
+     */
+    protected $createdIp;
+    
+    /**
+     * @var string $updatedIp
+     *
+     * @Gedmo\IpTraceable(on="update")
+     * @ORM\Column(type="string", name="updated_ip",length=45, nullable=true)
+     */
+    protected $updatedIp;
+
+    /**
+     * Set createdIp
+     *
+     * @param string $createdIp
+     * @return IpTraceableTrait
+     */
+    public function setCreatedIp($createdIp) 
+    {
+        $this->createdIp = $createdIp;
+
+        return $this;
+    }
+
+    /**
+     * Get createdIp
+     *
+     * @return string 
+     */
+    public function getCreatedIp() 
+    {
+        return $this->createdIp;
+    }
+
+    /**
+     * Set updatedIp
+     *
+     * @param string $updatedIp
+     * @return IpTraceableTrait
+     */
+    public function setUpdatedIp($updatedIp) 
+    {
+        $this->updatedIp = $updatedIp;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedIp
+     *
+     * @return string 
+     */
+    public function getUpdatedIp() 
+    {
+        return $this->updatedIp;
+    }
+}
