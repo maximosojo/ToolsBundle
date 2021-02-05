430a92e4a773b3530e3111d5ee093ea70b50e091
Máximo Sojo <maxsojo13@gmail.com>
2021-02-05 11:14:01 -0400

    refactor(FOSRest): Se agrega extension de vista rest.

---
+++ Component/FOS/RestBundle/View/FOSRestViewTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Máximo Sojo - maxtoan package.
+ * 
+ * (c) https://maximosojo.github.io/
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Maxtoan\ToolsBundle\Component\FOS\RestBundle\View;
+
+/**
+ * FOSRestView trait.
+ *
+ * @author Máximo Sojo <maxsojo13@gmail.com>
+ */
+trait FOSRestViewTrait
+{
+    /**
+     * Creates a view.
+     *
+     * Convenience method to allow for a fluent interface.
+     *
+     * @param mixed $data
+     * @param int   $statusCode
+     * @param array $headers
+     *
+     * @return \Maxtoan\ToolsBundle\Component\FOS\RestBundle\View\View
+     */
+    protected function view($data = null, $statusCode = null, array $headers = array())
+    {
+        return View::create($data, $statusCode, $headers);
+    }
+}
