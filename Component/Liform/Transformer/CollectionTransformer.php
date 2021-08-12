197c10b521878ea3fbbab155e42ff610414888ab
Máximo Sojo <maxsojo13@gmail.com>
2021-08-12 16:23:42 -0400

    refactor(Liform): Se agrega transformador para collections.

---
+++ Component/Liform/Transformer/CollectionTransformer.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Maxtoan\ToolsBundle\Component\Liform\Transformer;
+
+use Limenius\Liform\Exception\TransformerException;
+use Limenius\Liform\ResolverInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormTypeGuesserInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+use Limenius\Liform\Transformer\AbstractTransformer;
+
+/**
+ * @author Máximo Sojo <maxsojo13@gmail.com>
+ */
+class CollectionTransformer extends AbstractTransformer
+{
+    use \Maxtoan\ToolsBundle\Component\Liform\CommonFunctionsTrait;
+
+    /**
+     * @var ResolverInterface
+     */
+    protected $resolver;
+
+    /**
+     * @param TranslatorInterface           $translator
+     * @param FormTypeGuesserInterface|null $validatorGuesser
+     * @param ResolverInterface             $resolver
+     */
+    public function __construct(
+        TranslatorInterface $translator,
+        FormTypeGuesserInterface $validatorGuesser = null,
+        ResolverInterface $resolver
+    ) {
+        parent::__construct($translator, $validatorGuesser);
+        $this->resolver = $resolver;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform(FormInterface $form, array $extensions = [], $widget = null)
+    {
+        $this->initCommonCustom($form);
+
+        $children = [];
+        foreach ($form->all() as $name => $field) {
+            $transformerData = $this->resolver->resolve($field);
+            $transformedChild = $transformerData['transformer']->transform($field, $extensions, $transformerData['widget']);
+            $children[] = $transformedChild;
+
+            if ($transformerData['transformer']->isRequired($field)) {
+                $required[] = $field->getName();
+            }
+        }
+
+        if (empty($children)) {
+            $entryType = $form->getConfig()->getAttribute('prototype');
+
+            if (!$entryType) {
+                throw new TransformerException('Liform cannot infer the json-schema representation of a an empty Collection or array-like type without the option "allow_add" (to check the proptotype). Evaluating "'.$form->getName().'"');
+            }
+
+            $transformerData = $this->resolver->resolve($entryType);
+            $children[] = $transformerData['transformer']->transform($entryType, $extensions, $transformerData['widget']);
+            $children[0]['title'] = 'prototype';
+        }
+        
+        $schema = [
+            'type' => 'collection',
+            'title' => $form->getConfig()->getOption('label'),
+            'items' => $children,
+        ];
+
+        $schema = $this->addCommonSpecs($form, $schema, $extensions, $widget);
+
+        return $schema;
+    }
+}
