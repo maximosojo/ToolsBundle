1ffd7ec4549484d0b93025a43e0c333ad5ef56e3
Máximo Sojo <maxsojo13@gmail.com>
2022-08-16 18:53:39 -0400

    refactor(Common): Se actualiza dependencias.

--- Command/FolderClearCommand.php
+++ Command/FolderClearCommand.php
@@ -15,7 +15,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Formatter\OutputFormatterStyle;
-use Maximosojo\Common\Filesystem\Folder;
+use Maxtoan\Common\Filesystem\Folder;
 
 /**
  * FolderClearCommand
